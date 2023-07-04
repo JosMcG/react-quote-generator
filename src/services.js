efa88cddac44e15c71452c3f2d8f62cf1a9faca2
Josilyn McGuinness <josilyn_butler@yahoo.com>
2023-07-04 09:52:19 -0400

    included added files

---
+++ src/services.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqAWdaJKQ3ex3vglNuXoO_ZwCimmNdGtg",
+  authDomain: "random-quote-machine-f0ede.firebaseapp.com",
+  projectId: "random-quote-machine-f0ede",
+  storageBucket: "random-quote-machine-f0ede.appspot.com",
+  messagingSenderId: "338129003321",
+  appId: "1:338129003321:web:4063406511b2b6ceb23ee8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
